d9916d07d98ff1580960cadc0eeaad8459bcc01d
faatihat mohammed <105353034+001Pytom@users.noreply.github.com>
2023-03-24 10:43:42 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 """class city that inherits from BaseModel"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """has attributes state id and name"""
-    state_id = ""
-    name = ""
+    
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
