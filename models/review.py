d9788ab4de9d6e831b4020dd3ff95714804433e9
faatihat mohammed <105353034+001Pytom@users.noreply.github.com>
2023-03-24 11:12:13 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
-
 """review class inherits from basemodel"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
 class Review(BaseModel):
     """has attributes place_id, user_id and text"""
 
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
