da224f414ee0ff0928c3fd76c682b81c91e1ddd1
faatihat mohammed <105353034+001Pytom@users.noreply.github.com>
2023-03-24 11:13:51 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 """amenity class inherits from Basemodel"""
 
 
 class Amenity(BaseModel):
     """has just a name attribute"""
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
