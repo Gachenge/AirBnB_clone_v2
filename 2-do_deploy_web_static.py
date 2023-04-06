d007d98097222eea843f1f705f378b55af790608
Gachenge <gachenge1@gmail.com>
2023-04-06 18:24:04 +0300

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,16 +13,13 @@ def do_deploy(archive_path):
     """deploy web static to the servers"""
     if not path.exists(archive_path):
         return False
-    try:
-        put(archive_path, '/tmp')
-        file = path.basename(archive_path)
-        ext = path.splitext(file)[0]
-        run('mkdir -p /data/web_static/releases/{}/'.format(ext))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file, ext))
-        run('rm -rf /tmp/{}'.format(file))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(ext))
-        return True
-    except Exception:
-        return False
-    
+    put(archive_path, '/tmp')
+    file = path.basename(archive_path)
+    ext = path.splitext(file)[0]
+    run('mkdir -p /data/web_static/releases/{}/'.format(ext))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file, ext))
+    run('rm -rf /tmp/{}'.format(file))
+    run('sudo rm -rf /data/web_static/current')
+    run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'
+        .format(ext))
+    return True
