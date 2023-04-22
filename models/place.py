627fe654c0ee2883986f06df32db9a740639bf2c
Gachenge <gachenge1@gmail.com>
2023-04-22 20:40:54 +0300

    overlaps

--- models/place.py
+++ models/place.py
@@ -46,8 +46,8 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
 
     reviews = relationship("Review", backref="place", cascade="delete")
-    amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False)
+    amenities = relationship ("Amenity", secondary="place_amenity",
+                             viewonly=False, overlaps="place_amenities")
 
     amenity_ids = []
 
