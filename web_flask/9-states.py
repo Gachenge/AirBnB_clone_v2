59ea5c3396d8723201da597029d642581ad45c40
Gachenge <gachenge1@gmail.com>
2023-04-23 23:15:08 +0300

    by state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -29,7 +29,7 @@ def state(id):
         if states.id == id:
             return render_template("9-states.html", states=states, id='yes')
     return render_template("9-states.html")
-        
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', debug=None)
