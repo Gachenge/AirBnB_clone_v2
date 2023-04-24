ff911b348e47985860ac4574e712ff02bd9b7bf2
Gachenge <gachenge1@gmail.com>
2023-04-24 18:55:46 +0300

    documentation

--- models/state.py
+++ models/state.py
@@ -15,10 +15,13 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="delete",
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="delete",
                           backref="states")
+    else:
+        name = ""
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
